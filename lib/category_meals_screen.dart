ad3c4cf8dc7cd15021e12604809701474e5333ca
Mehmet Yılmaz <mehmetyilmaz001@gmail.com>
2019-09-29 01:38:22 +0300

    Routing between screens

---
+++ lib/category_meals_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import './dummy_data.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  static const routeName = '/category-meals';
+  // final String categoryId;
+  // final String categoryTitle;
+
+  // CategoryMealsScreen(this.categoryId, this.categoryTitle);
+
+  @override
+  Widget build(BuildContext context) {
+    final routeArg = ModalRoute.of(context).settings.arguments as Map<String, String>;
+    final categoryTitle = routeArg['title'];
+    final categoryId = routeArg['id'];
+    final categoryMeals = DUMMY_MEALS.where((meal){
+      return meal.categories.contains(categoryId);
+    }).toList();
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(categoryTitle),
+        ),
+        body: ListView.builder(itemBuilder: (ctx, i){
+          return Text(categoryMeals[i].title); 
+        }, itemCount:  categoryMeals.length,));
+  }
+}
