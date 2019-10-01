bf62ca18b84279e5f618ded9e7e6dde7124b98c2
Admin <admin@KS-MacBook-Pro.local>
2019-10-01 07:44:07 +0300

    replacing screens instead of pushing

--- lib/screens/filters_screens.dart
+++ lib/screens/filters_screens.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import '../widgets/main_drawer.dart';
 
 class FiltersScreen extends StatelessWidget {
   static const routeName = 'filters-screen';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: MainDrawer(),
       appBar: AppBar(title: Text('Filters'),),
           body: Container(
         child: Text('Filters'),
--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -38,10 +38,10 @@ class MainDrawer extends StatelessWidget {
             height: 20,
           ),
           _buildListTile('Meals', Icons.restaurant, (){
-            Navigator.of(context).pushNamed('/');
+            Navigator.of(context).pushReplacementNamed('/');
           }),
           _buildListTile('Filters', Icons.settings, () {
-            Navigator.of(context).pushNamed(FiltersScreen.routeName);
+            Navigator.of(context).pushReplacementNamed(FiltersScreen.routeName);
           }),
           
         ],
